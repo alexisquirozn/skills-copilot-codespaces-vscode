22f9411804006dad935e3c4d0d38b6c43fa58232
alexisquirozn <141955634+alexisquirozn@users.noreply.github.com>
2023-08-12 13:41:59 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,12 @@
+function skillsMember(){
+    var member = document.getElementById("member");
+    var skill = document.getElementById("skill");
+    var memberbtn = document.getElementById("memberbtn");
+    var skillbtn = document.getElementById("skillbtn");
+    member.style.display = "block";
+    skill.style.display = "none";
+    memberbtn.style.backgroundColor = "#fff";
+    memberbtn.style.color = "#000";
+    skillbtn.style.backgroundColor = "#000";
+    skillbtn.style.color = "#fff";
+}
